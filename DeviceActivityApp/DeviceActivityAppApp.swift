09d8f658d74d66b80b938dc3e3385a6af34c913e
Pedro Victor Somensi <psomensi.contato@gmail.com>
2023-08-06 15:44:14 -0300

    initial commit

---
+++ DeviceActivityApp/DeviceActivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceActivityAppApp.swift
+//  DeviceActivityApp
+//
+//  Created by Pedro Somensi on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DeviceActivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
